c6ced8600b05c740433e645a0566ad59682e2de9
lzc <lzc.lh@foxmail.com>
2024-12-05 22:52:37 +0800

    music go web

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	// 创建 gin 引擎
 	r := gin.Default()
 
-	// 定义路由
+	// 创建一个静态文件服务，用于提供音频文件
+	r.Static("/static", "./static")
+
+	// 主页面，返回一个简单的 HTML 页面，其中包含音频播放器
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello Windsurf!",
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Welcome to Music Player",
 		})
 	})
 
+	// 设置 HTML 模板目录
+	r.LoadHTMLGlob("templates/*")
+
+	log.Println("服务器启动在 :8081 端口...")
 	// 启动服务器
 	r.Run(":8081")
 }
